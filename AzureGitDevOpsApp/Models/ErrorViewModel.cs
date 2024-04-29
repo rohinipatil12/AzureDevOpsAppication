876159c47cd3f5664ff55d5ff6c0d305c74690cb
Rohini Patil <rohinipatild36@gmail.com>
2024-04-28 19:35:27 -0600

    Commiting the Code Structure

---
+++ AzureGitDevOpsApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureGitDevOpsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
