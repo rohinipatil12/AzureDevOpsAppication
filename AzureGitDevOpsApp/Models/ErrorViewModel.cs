9ee00e30aad6684f0c06d8d2df14f0b6243319e5
rohinipatil12 <107148051+rohinipatil12@users.noreply.github.com>
2024-05-08 15:37:56 -0600

    Updating the model in the Main branch

--- AzureGitDevOpsApp/Models/ErrorViewModel.cs
+++ AzureGitDevOpsApp/Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace AzureGitDevOpsApp.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+        public int Id   { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
